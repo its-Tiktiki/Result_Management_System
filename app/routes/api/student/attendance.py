c71e00d5a41df4bb54bdae1d81ceca35344b5018
rakibhossendev <rh01828315879@gmail.com>
2026-09-15 23:13:01 +0600

    Add APIS

---
+++ app/routes/api/student/attendance.py
@@ -0,0 +1,31 @@
+from flask import jsonify,Blueprint
+from app.models.teacher import Attendance
+from app.models.assign import Subjects
+
+attendance_api_bp = Blueprint(
+    "attendance_api",
+    __name__,
+    url_prefix="/attendance/api/"
+)
+
+@attendance_api_bp.route("/",methods=["GET"])
+def attendance_api():
+    student_data = Attendance.query.all()
+
+    data = []
+
+    for student in student_data:
+        data.append({
+            "attendance_id": student.attendance_id,
+            "student_id": student.student_id,
+            "student_roll": student.student.student_roll,
+            "subject_name": student.subject.subject_name,
+            "attendance_date": student.attendance_date.isoformat(),
+            "status": student.status,  
+        })
+
+    return jsonify({
+        "success": True,
+        "total": len(data),
+        "data": data,
+    })
